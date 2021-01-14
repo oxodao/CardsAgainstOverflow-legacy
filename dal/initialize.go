0cd758c83e9cc6016183c827205d373dc204fa58
Nathan JANCZEWSKI <nathan@janczewski.fr>
2021-01-14 15:41:35 +0100

    Fixing issue connecting to DB on prod

--- dal/initialize.go
+++ dal/initialize.go
@@ -17,10 +17,28 @@ type jsonInputFile struct {
 
 var dbSingleton *sqlx.DB
 
+func envOrDefault(env, def string) string {
+	a := os.Getenv(env)
+	if len(a) == 0 {
+		return def
+	}
+
+	return a
+}
+
 func GetDatabase() *sqlx.DB {
 	var err error = nil
 	if dbSingleton == nil {
-		dbSingleton, err = sqlx.Connect("postgres", "postgres://cao:cao@localhost:5432/cao?sslmode=disable")
+
+	    username := envOrDefault("DB_USERNAME", "cao")
+	    password := envOrDefault("DB_PASSWORD", "cao")
+	    host := envOrDefault("DB_HOST", "localhost")
+	    port := envOrDefault("DB_PORT", "5432")
+		database := envOrDefault("DB_DATABASE", "cao")
+
+		dsn := fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=disable", username, password, host, port, database)
+
+		dbSingleton, err = sqlx.Connect("postgres", dsn)
 		if err != nil {
 			panic(err)
 		}
